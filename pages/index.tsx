6ff8055b90beee77e30c07b07e92c9d3d71f3192
noataso <noa740740@outlook.jp>
2023-02-19 19:42:06 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Home.module.scss'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import SelfImage from "../Images/SelfImage.png";
+import { motion } from "framer-motion";
+import { ReactNode, useRef, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [isDark,setIsDark]=useState(false);
+  const handleToggleDark=(e:React.ChangeEvent<HTMLInputElement>)=>{
+    setIsDark(!isDark);
+  }
   return (
     <>
       <Head>
@@ -14,110 +23,56 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
+      <div className={isDark? `${styles.darkTheme}`:`null`}
+      style={{transition:"1s"}}></div>
+      <div>
+        <Header isDark={isDark} />
+        <input className={styles.toggler} onChange={(e)=>handleToggleDark(e)} type="checkbox" id="switch" name="mode" />
+        <label htmlFor="switch" className={styles.switch}></label>
+        <motion.div
+        className='m-4 p-4 bg-black text-white'
+        style={{borderRadius:"10px",height:"100%"}}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        >
+          <h1>自己紹介</h1>
+          <motion.div
+          initial={{opacity:0,x:"-50%"}}
+          animate={{opacity:1,x:0}}
+          transition={{duration:0.5}}>
+            <Image src={SelfImage} className="mt-0 mb-4"
+            style={{
+              width: "300px",
+              height: "300px",
+              borderRadius: "50%",
+              marginLeft: "50%",
+              transform: "translateX(-50%)"
+            }} alt={''} ></Image>
+          </motion.div>
+          <h2>趣味・特技</h2>
+          <p>私の特技は長距離走です。練習を積み重ねて大学記録を出したことがあります。
+            またペースメーカーなど人の役に立てることに生きがいを感じます。
+            趣味はプログラミングで、モノづくりをしたときにやりがいを感じます。
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+          <h2>学生時代に力を入れたこと</h2>
+          <p>学部では、振動、音響に関する研究室に属していて、私は振動に関する研究をしていました。内容は、建物内で人が歩行したときの振動加速度を測定して、これを解析ソフトで作成した3Dモデル内で人の歩行を模擬した荷重をあたえることによって得られた振動加速度と一致させることを目標としていて、これにより人の歩行振動から個人の特定を可能にし、。解析には、建物の物性値などの条件を設定する必要がありました。設定した条件に対して卒論提出の1週間前まで細かい追及をしてきました。結果として、自分の納得いくような研究成果を出すことができました。学生生活の4年間で、論文を書くことなど貴重な経験を積むことができたのもよい経験となりました。</p>
+          <p>大学院では、研究室を変えて軸受に関する研究を行っています。私はその中で、電食に関する研究を行っており、エアコン内蔵の小型モータなどに多く導入されている小型玉軸受について、モータから軸に流れた漏れ電流によって内輪と外輪の間に電位差が発生し放電が起きた時に、損傷した軸受の上昇した振動のデータから、振動上昇の予測することを目標としています。ある条件設定において、ある一定の損傷及び振動上昇を発生させることができれば、振動周波数から損傷具合を特定することも可能になり、機械システムの損傷の早期防止にもつながります。3つの実験装置を使い、グリースよりも軸受内の油膜厚さが均一とされる油潤滑下でそれぞれの電食による振動上昇を測定しています。</p>
+          <h2>自分の長所</h2>
+          <p>私の長所は一度立てた目標に対してあきらめずに継続して
+            努力できることです。私は10年間続けてきた陸上競技で継続力
+            を養うことができました。大学時代では大学記録を作ることや
+            学長に表彰されるなど目標を叶えてきました。しかし目標を
+            叶えるためにより早く高い結果を求めすぎて休みを入れずに練習
+            したことで怪我や不調に悩まされました時期もありました。そこで
+            私はこの経験から調子がいい時こそ追い込みすぎないことを意識
+            するようになり、結果を継続的に出すことができるようになりました。
+            社会人になってもとにかく継続を大事にして、多くの目標を叶えて
+            いきたいと思います。
+          </p>
+        </motion.div>
+        <Footer isDark={isDark} />
+      </div>
     </>
   )
 }
