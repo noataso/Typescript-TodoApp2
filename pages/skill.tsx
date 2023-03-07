7a46c33b0e0ae9a348b51601995297e1b3af775d
noataso <noa740740@outlook.jp>
2023-03-07 21:27:52 +0900

    変更

--- pages/skill.tsx
+++ pages/skill.tsx
@@ -48,108 +48,101 @@ const skill = () => {
                 <h1 className="mb-4">プログラミング経験</h1>
                 <h2 className="mt-3">プログラミング言語</h2>
                 <div className="row">
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>HTML</h3>
                         <Image src={Html} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>CSS</h3>
                         <Image src={Css} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Javascript</h3>
                         <Image src={Javascript} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                </div>
-                <div className="row">
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Java</h3>
                         <Image src={Java} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Python</h3>
                         <Image src={Python} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Ruby</h3>
                         <Image src={Ruby} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                </div>
-                <div className="row">
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Sass</h3>
                         <Image src={Sass} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                     </div>
                 </div>
+                
                 <h2 className="mt-3">データベース</h2>
                 <div className="row">
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Firebase</h3>
                         <Image src={Firebase} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Mysql</h3>
                         <Image src={Mysql} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Mongodb</h3>
                         <Image src={Mongodb} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                </div>
-                <div className="row">
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Postgresql</h3>
                         <Image src={Postgresql} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Prisma</h3>
                         <Image src={Prisma} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                     </div>
                 </div>
                 <h2 className="mt-3">フレームワーク</h2>
                 <div className="row">
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Node.js</h3>
                         <Image src={Nodejs} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Nextjs</h3>
                         <Image src={Nextjs} style={{ width: "150px", height: "150px",borderRadius:"50%" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>React</h3>
                         <Image src={React} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                </div>
-                <div className="row">
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Django</h3>
                         <Image src={Django} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Bootstrap</h3>
                         <Image src={Bootstrap} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Materialui</h3>
                         <Image src={Materialui} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
                 </div>
                 <h2 className="mt-3">その他</h2>
                 <div className="row">
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                         <h3>Github</h3>
                         <Image src={Github} style={{ width: "150px", height: "150px" }} alt={""} />
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                     </div>
-                    <div className="container mt-3 col-3">
+                    <div className="container mt-3 col-lg-4 col-md-6">
                     </div>
                 </div>
             </motion.div>
